5acc8ab50a2688c7748d78ba15512365069a950b
Emma Bautista <emmabautista.2008@gmail.com>
2025-10-12 18:54:11 +0000

    game

--- game.js
+++ game.js
@@ -3,7 +3,7 @@
 //Background on the game ! 
 kaboom({
     width: 1400,
-    height: 800,
+    height: 707,
     background: [234, 205, 159],
     // background: [116, 71, 0],
 });
@@ -23,6 +23,8 @@ loadSound("backgroundMusic", "/Background.mp3");
 loadSound("boomMusic", "/BoomMusic.mp3 ");
 loadSound("CoinMusic", "/Coin.mp3");
 loadSound("GameOver", "/GameOver.mp3");
+loadSprite("lighting", "https://kaboomjs.com/sprites/lightening.png");
+
 
 // --- Global background music ---
 
@@ -205,24 +207,33 @@ scene("main", ({ level } = { level: 0 }) => {
 loadSprite("boom", "https://kaboomjs.com/sprites/kaboom.png");
 
     player.onCollide("enemy", (enemy, col) => {
-        if (col.isBottom()) {
-            play("boomMusic");
-            destroy(enemy);
-            player.jump(300);
+    if (col.isBottom()) {
+        play("boomMusic");
+        destroy(enemy);
+        player.jump(300);
 
+        add([
+            sprite("boom"),
+            pos(enemy.pos),
+            lifespan(0.5), 
+            scale(1), 
+        ]);
+    } else {
+        destroy(player);
+        play("GameOver", { volume: 3 }); // correct syntax 
 
-            add([
-                sprite("boom"),
-                pos(enemy.pos),
-                lifespan(0.5), 
-                scale(1), 
-                ]);
-        } else {
-            destroy(player);
-            play("GameOver");
-            wait(2, () => go("lose"));
-        }
-    });
+       
+        add([
+            sprite("lighting"), 
+            pos(enemy.pos),
+            lifespan(2.4),
+            scale(2),
+        ]);
+
+        // Wait 2 seconds then go to lose scene
+        wait(2, () => go("lose"));
+    }
+});
 
 player.onCollide("door", () => {
     const nextLevel = currentLevel + 1;
@@ -241,9 +252,24 @@ player.onCollide("door", () => {
 scene("win", () => {
     add([ text("You Win!"), pos(center()), anchor("center") ]);
     wait(2, () => { go("main", { level: 0 }); });
+
 });
 scene("lose", () => {
-    add([ text("Game Over"), pos(center()), anchor("center") ]);
+    const mid = center(); 
+
+    const line1 = add([
+        text("Game Over", { size: 48 }), 
+        pos(mid),
+        anchor("center"),
+    ]);
+    add([
+        text("(imagine losing lol)", { size: 24 }),
+        pos(mid.x, mid.y + line1.height / 2 + 10), 
+
+    // add([ text("Game Over"), pos(center()), anchor("center") ]);
+    // add([ text("(imagine losing lol)"), pos(center().x, center().y + 40), anchor("center") 
+    
+]);
     wait(3, () => { go("main", { level: 0 }); });
 });
 
